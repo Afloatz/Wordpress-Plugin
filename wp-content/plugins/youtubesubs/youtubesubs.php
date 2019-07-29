a0423308588fc1790f48529d2bf1eab1d0b6eb54
Afloatz <florentlaot.fr@gmail.com>
2019-07-29 09:36:55 +0200

    Plugin access security

--- wp-content/plugins/youtubesubs/youtubesubs.php
+++ wp-content/plugins/youtubesubs/youtubesubs.php
@@ -7,3 +7,10 @@ Version: 1.0.0
 Author: Florent Laot
 Author URI: https://florent-laot.fr/
 */
+
+// Exit if accessed directly (if the directory path is entered in the url, it won't display the content)
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+echo 123;
