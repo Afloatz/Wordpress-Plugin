456d72dbd2d9ed089816a41c63417320ef875c67
Afloatz <florentlaot.fr@gmail.com>
2019-07-29 09:25:00 +0200

    Plugin header

---
+++ wp-content/plugins/youtubesubs/youtubesubs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Youtube Subs
+Plugin URI: https://florent-laot.fr/
+Description: Display Youtube sub button and count
+Version: 1.0.0
+Author: Florent Laot
+Author URI: https://florent-laot.fr/
+*/
